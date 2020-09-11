03dc6f2c9f691376cf55a38366391cf06e3ec306
jx <1541223540@qq.com>
2020-09-11 14:51:51 +0800

    '1'

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="http://www.mrrax.cn" target="_blank">JX</a> 编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:1541223540@qq.com">联系我们</a></p>
+    </div>
+</footer>
